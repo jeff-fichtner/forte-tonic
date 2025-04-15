7827efd141eb3bb9bc1451edf6b0b7e6d8e11033
Jeff Fichtner <jeff.fichtner@gmail.com>
2025-04-15 02:00:44 -0700

    Add controls, listeners

--- src/main.js
+++ src/main.js
@@ -1,3 +1,29 @@
-function doGet() {
-  return HtmlService.createHtmlOutputFromFile('index');
+function doGet(request) {
+  return HtmlService.createTemplateFromFile('index')
+      .evaluate();
+}
+
+function include(filename) {
+  return HtmlService.createHtmlOutputFromFile(filename)
+      .getContent();
+}
+
+function getStudentsForEmail(email) {
+  return email;
+}
+
+function getCurrentLessonsForStudent(studentId) {
+  return studentId;
+}
+
+function addLessonForStudent(studentId, lessonId) {
+  return studentId;
+}
+
+function deleteLessonForStudent(studentId, lessonId) {
+  return studentId;
+}
+
+function loadLessonsForInstrument(studentId, instrumentId) {
+  return studentId;
 }
