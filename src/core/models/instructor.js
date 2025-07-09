3d5a1e114dc28b6e09bb5d64ef2a4b27dc333c11
Jeff Fichtner <jeff.fichtner@gmail.com>
2025-07-09 00:38:59 -0700

    Adds instructor availability to model
    Adds boolean flags for instructor availability on each day of the week to the instructor model.
    
    This enables filtering available instructors when creating new registrations.
    Also, includes luxon library and durationHelpers file.

--- src/core/models/instructor.js
+++ src/core/models/instructor.js
@@ -13,18 +13,23 @@ class Instructor {
         instrument2,
         instrument3,
         instrument4,
+        isAvailableMonday,
         mondayStartTime,
         mondayEndTime,
         mondayRoomId,
+        isAvailableTuesday,
         tuesdayStartTime,
         tuesdayEndTime,
         tuesdayRoomId,
+        isAvailableWednesday,
         wednesdayStartTime,
         wednesdayEndTime,
         wednesdayRoomId,
+        isAvailableThursday,
         thursdayStartTime,
         thursdayEndTime,
         thursdayRoomId,
+        isAvailableFriday,
         fridayStartTime,
         fridayEndTime,
         fridayRoomId) {
@@ -41,18 +46,23 @@ class Instructor {
         this.instrument2 = instrument2;
         this.instrument3 = instrument3;
         this.instrument4 = instrument4;
+        this.isAvailableMonday = isAvailableMonday;
         this.mondayStartTime = mondayStartTime;
         this.mondayEndTime = mondayEndTime;
         this.mondayRoomId = mondayRoomId;
+        this.isAvailableTuesday = isAvailableTuesday;
         this.tuesdayStartTime = tuesdayStartTime;
         this.tuesdayEndTime = tuesdayEndTime;
         this.tuesdayRoomId = tuesdayRoomId;
+        this.isAvailableWednesday = isAvailableWednesday;
         this.wednesdayStartTime = wednesdayStartTime;
         this.wednesdayEndTime = wednesdayEndTime;
         this.wednesdayRoomId = wednesdayRoomId;
+        this.isAvailableThursday = isAvailableThursday;
         this.thursdayStartTime = thursdayStartTime;
         this.thursdayEndTime = thursdayEndTime;
         this.thursdayRoomId = thursdayRoomId;
+        this.isAvailableFriday = isAvailableFriday;
         this.fridayStartTime = fridayStartTime;
         this.fridayEndTime = fridayEndTime;
         this.fridayRoomId = fridayRoomId;
@@ -60,25 +70,5 @@ class Instructor {
 
     get fullName() {
         return `${this.firstName} ${this.lastName}`;
-    }
-
-    get isAvailableMonday() {
-        return this.mondayStartTime && this.mondayEndTime;
-    }
-
-    get isAvailableTuesday() {
-        return this.tuesdayStartTime && this.tuesdayEndTime;
-    }
-
-    get isAvailableWednesday() {
-        return this.wednesdayStartTime && this.wednesdayEndTime;
-    }
-
-    get isAvailableThursday() {
-        return this.thursdayStartTime && this.thursdayEndTime;
-    }
-
-    get isAvailableFriday() {
-        return this.fridayStartTime && this.fridayEndTime;
     }
 }
